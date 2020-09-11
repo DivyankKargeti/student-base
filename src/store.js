5580a591395e463c958a4709d07799ea95095f18
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-09-11 16:47:11 +0530

    store.js firebase creds hidden

--- src/store.js
+++ src/store.js
@@ -10,14 +10,7 @@ import {
 import {composeWithDevTools} from "redux-devtools-extension";
 import { createFirestoreInstance, firestoreReducer } from 'redux-firestore';// <- needed if using firestore
 const fbConfig = {
-    apiKey: "AIzaSyBole420GrfqvA-9Q5H8GQ125AlNZ6iTTg",
-    authDomain: "student-base-343d3.firebaseapp.com",
-    databaseURL: "https://student-base-343d3.firebaseio.com",
-    projectId: "student-base-343d3",
-    storageBucket: "student-base-343d3.appspot.com",
-    messagingSenderId: "776349308278",
-    appId: "1:776349308278:web:e008caf98ae361f7765657",
-    measurementId: "G-Q99PT463JB"
+    //PUT YOUR FIREBASE CONFIGURATIONS HERE FROM FIREBASE CONSOLE
 };
  
 // react-redux-firebase config
